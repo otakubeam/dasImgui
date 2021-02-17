9afb0c9ccf85b9322f3046d9704c09d7d41bf8bf
Boris Batkin <bbatkin@gmail.com>
2021-02-16 21:13:33 -0800

    | or |= is now a thing for some generated enums because sparta

--- src/module_imgui_enums.cpp
+++ src/module_imgui_enums.cpp
@@ -15,25 +15,6 @@ using namespace das;
 
 #endif
 
-
-namespace das {
-
-    template <typename ET>
-    struct das_operator_enum_OR {
-        static ET invoke ( ET a, ET b ) {
-            return ET ( uint64_t(a) | uint64_t(b) );
-        }
-    };
-
-    template <typename ET>
-    inline auto addEnumFlagOps ( Module & mod, ModuleLibrary & lib, const string & cppName ) {
-        using method_or = das_operator_enum_OR<ET>;
-        return addExtern<DAS_CALL_METHOD(method_or)>(mod, lib, "|", SideEffects::none,
-            ("das_operator_enum_OR<" + cppName + ">::compute").c_str());
-    }
-
-}
-
 void Module_imgui::initEnums() {
 #if USE_GENERATED
 #include "module_imgui.enum.inc"
