7bbe1e1d3652a49c4bbf27c8708df42ad5cc9423
Boris Batkin <bbatkin@gmail.com>
2021-04-29 14:05:36 -0700

    now with basic interractions and some boost

--- src/module_imgui_node_editor.cpp
+++ src/module_imgui_node_editor.cpp
@@ -211,13 +211,17 @@ public:
                 SideEffects::worstDefault, "ax::NodeEditor::GetNodeBackgroundDrawList");
 
         addExtern<DAS_BIND_FUN(ax::NodeEditor::Link)>(*this, lib, "Link",
-                SideEffects::worstDefault, "ax::NodeEditor::Link");
+                SideEffects::worstDefault, "ax::NodeEditor::Link")
+                ->arg_init(3, make_smart<ExprConstFloat4>(float4(1.0f)))
+                ->arg_init(4, make_smart<ExprConstFloat>(1.0f));
 
         addExtern<DAS_BIND_FUN(ax::NodeEditor::Flow)>(*this, lib, "Flow",
                 SideEffects::worstDefault, "ax::NodeEditor::Flow");
 
         addExtern<DAS_BIND_FUN(ax::NodeEditor::BeginCreate)>(*this, lib, "BeginCreate",
-                SideEffects::worstDefault, "ax::NodeEditor::BeginCreate");
+                SideEffects::worstDefault, "ax::NodeEditor::BeginCreate")
+                ->arg_init(0, make_smart<ExprConstFloat4>(float4(1.0f)))
+                ->arg_init(1, make_smart<ExprConstFloat>(1.0f));
         addExtern<bool (*)(ax::NodeEditor::PinId *,ax::NodeEditor::PinId *),ax::NodeEditor::QueryNewLink>(*this, lib, "QueryNewLink",
                 SideEffects::worstDefault, "ax::NodeEditor::QueryNewLink");
         addExtern<bool (*)(ax::NodeEditor::PinId *, ax::NodeEditor::PinId *, const ImVec4&, float) ,ax::NodeEditor::QueryNewLink>(*this, lib, "QueryNewLink",
