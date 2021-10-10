a93b93490edd331012d19ef5a983db978730cd93
Boris Batkin <bbatkin@gmail.com>
2021-10-10 00:35:32 -0700

    correct binding for AOT

--- src/aot_imgui_node_editor.h
+++ src/aot_imgui_node_editor.h
@@ -1,9 +1,21 @@
 #pragma once
 
 namespace das {
-   template <> struct das_alias<ax::NodeEditor::PinId> : das_alias_ref<ax::NodeEditor::PinId,int32_t> {};
-   template <> struct das_alias<ax::NodeEditor::LinkId> : das_alias_ref<ax::NodeEditor::LinkId,int32_t> {};
-   template <> struct das_alias<ax::NodeEditor::NodeId> : das_alias_ref<ax::NodeEditor::NodeId,int32_t> {};
+   template <> struct das_alias<ax::NodeEditor::PinId> : das_alias_ref<ax::NodeEditor::PinId,int32_t> {
+        static __forceinline ax::NodeEditor::PinId to ( int32_t value ) {
+            return ax::NodeEditor::PinId(value);
+        }
+   };
+   template <> struct das_alias<ax::NodeEditor::LinkId> : das_alias_ref<ax::NodeEditor::LinkId,int32_t> {
+        static __forceinline ax::NodeEditor::LinkId to ( int32_t value ) {
+            return ax::NodeEditor::LinkId(value);
+        }
+   };
+   template <> struct das_alias<ax::NodeEditor::NodeId> {
+        static __forceinline ax::NodeEditor::NodeId to ( int32_t value ) {
+            return ax::NodeEditor::NodeId(value);
+        }
+   };
 }
 
 DAS_BIND_ENUM_CAST(ax::NodeEditor::SaveReasonFlags);
